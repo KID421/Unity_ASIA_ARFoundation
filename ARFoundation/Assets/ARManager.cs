7c0698d352643fff13306c3a798c50b13c0b254a
KID421 <50666207+KID421@users.noreply.github.com>
2020-11-24 11:18:06 +0800

    AR 管理器程式

--- ARFoundation/Assets/ARManager.cs
+++ ARFoundation/Assets/ARManager.cs
@@ -1,5 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;  // 引用 Foundation API
 
+/// <summary>
+/// 點擊地面生成物件
+/// </summary>
+// RC 要求元件：在第一次套用腳本時執行
+[RequireComponent(typeof(ARRaycastManager))]
 public class ARManager : MonoBehaviour
 {
 
