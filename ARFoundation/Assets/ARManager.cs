813c3b25d8da2e42c51fd64469477bf1bf820fa0
KID421 <50666207+KID421@users.noreply.github.com>
2020-11-24 11:46:53 +0800

    取得玩家滑鼠資訊

--- ARFoundation/Assets/ARManager.cs
+++ ARFoundation/Assets/ARManager.cs
@@ -18,4 +18,24 @@ public class ARManager : MonoBehaviour
     private Vector2 pointMouse;
     // 碰撞資訊
     private List<ARRaycastHit> hits;
+
+    /// <summary>
+    /// 點擊
+    /// </summary>
+    private void Tap()
+    {
+        // 判斷玩家是否點擊
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            pointMouse = Input.mousePosition;           // 滑鼠座標 = 玩家的滑鼠座標
+            print(pointMouse);                          // 輸出座標測試用
+        }
+        // 判斷射線是否打到物件
+        // 生成物件
+    }
+
+    private void Update()
+    {
+        Tap();
+    }
 }
