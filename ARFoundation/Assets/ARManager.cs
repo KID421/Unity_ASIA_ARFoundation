d7c0a8e7109b66849f1d763605cff29909060d40
KID421 <50666207+KID421@users.noreply.github.com>
2020-11-24 11:30:31 +0800

    添加欄位

--- ARFoundation/Assets/ARManager.cs
+++ ARFoundation/Assets/ARManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.XR.ARFoundation;  // 引用 Foundation API
+using System.Collections.Generic;   // 引用 系統.集合.一般 包含清單 List
 
 /// <summary>
 /// 點擊地面生成物件
@@ -8,5 +9,13 @@ using UnityEngine.XR.ARFoundation;  // 引用 Foundation API
 [RequireComponent(typeof(ARRaycastManager))]
 public class ARManager : MonoBehaviour
 {
+    [Header("點擊後要生成的物件")]
+    public GameObject obj;
+    [Header("AR 管理器")]
+    public ARRaycastManager arManager;
 
+    // 滑鼠座標
+    private Vector2 pointMouse;
+    // 碰撞資訊
+    private List<ARRaycastHit> hits;
 }
